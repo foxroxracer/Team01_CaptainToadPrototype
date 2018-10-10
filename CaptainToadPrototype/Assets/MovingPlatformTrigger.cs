e426f73b5035fcb5c06be7a12b7a645832833997
Malamuty <sanderdrupal@gmail.com>
2018-10-10 22:28:28 +0200

    - Added basic moving platform mechanic

---
+++ CaptainToadPrototype/Assets/MovingPlatformTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformTrigger : MonoBehaviour {
+
+    public Mechanic_MovingPlatform MovingPlatformMechanic;
+    private bool _mainCharacterOnPlatform = false;
+
+    void Update() {
+        if (_mainCharacterOnPlatform && AButtonPressed()) {
+            MovingPlatformMechanic.TogglePlatformMovement();
+        }    
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "MainCharacter") {
+            PutMainCharacterOnPlatform(other.gameObject.transform);
+            _mainCharacterOnPlatform = true;
+        }        
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "MainCharacter") {
+            RemoveMainCharacterFromPlatform(other.gameObject.transform);
+            _mainCharacterOnPlatform = false;
+        }
+    }
+
+    private void PutMainCharacterOnPlatform(Transform mainCharacter) {
+        mainCharacter.transform.parent = MovingPlatformMechanic.Platform;
+    }
+
+    private void RemoveMainCharacterFromPlatform(Transform mainCharacter) {
+        mainCharacter.transform.parent = null;
+    }
+
+    private bool AButtonPressed() {
+        return Input.GetButtonDown("A");
+    }
+}
---
+++ CaptainToadPrototype/Assets/Scripts/Mechanics/Mechanic_MovingPlatform.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mechanic_MovingPlatform : MonoBehaviour {
+    [Header("Setup")]
+    public Transform StartPoint;
+    public Transform EndPoint;
+    public Transform Platform;
+
+    [Header("Settings")]
+    public float TravelTimeinSeconds = 3f;
+    public AnimationCurve MovementCurve;
+
+    private enum PlatformState {
+        Up,
+        Down,
+    }
+    private PlatformState _currentPlatformState = PlatformState.Down;
+    private bool _platformIsMoving = false;
+    
+    void Start() {
+        MovePlatformToStartPoint();        
+    }
+
+    public void TogglePlatformMovement() {
+        if (!_platformIsMoving) {
+            if (_currentPlatformState == PlatformState.Up) {
+                MovePlatformDown();
+                _currentPlatformState = PlatformState.Down;
+            } else {
+                MovePlatformUp();
+                _currentPlatformState = PlatformState.Up;
+            }
+        }
+    }
+
+    private void MovePlatformUp() {
+        StartCoroutine(MovePlatform(PlatformState.Up));
+    }
+
+    private void MovePlatformDown() {
+        StartCoroutine(MovePlatform(PlatformState.Down));
+    }
+
+    IEnumerator MovePlatform(PlatformState direction) {
+        _platformIsMoving = true;
+        float elapsedTime = 0f;
+
+        Vector3 startPoint = this.StartPoint.position;
+        Vector3 endPoint = this.EndPoint.position;
+
+        if (direction == PlatformState.Down) {
+            startPoint = this.EndPoint.position;
+            endPoint = this.StartPoint.position;
+        }
+
+        while (elapsedTime < TravelTimeinSeconds) {
+            float fractionFromCurve = MovementCurve.Evaluate((elapsedTime / TravelTimeinSeconds));
+            Platform.transform.position = Vector3.Lerp(startPoint, endPoint, fractionFromCurve);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Platform.transform.position = endPoint;
+
+        _platformIsMoving = false;
+    }
+
+    private void MovePlatformToStartPoint() {
+        Platform.position = StartPoint.position;
+    }
+}
