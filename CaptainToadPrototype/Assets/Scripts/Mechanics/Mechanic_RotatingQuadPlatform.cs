098d6faaf12ff4dd7d75be5c0959518d7e4a0948
Malamuty <sanderdrupal@gmail.com>
2018-10-11 10:08:08 +0200

    - Added RotatingQuadPlatform mechanic

---
+++ CaptainToadPrototype/Assets/Scripts/Mechanics/Mechanic_RotatingQuadPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mechanic_RotatingQuadPlatform : MonoBehaviour {
+    public Transform RotatingQuadPlatform;
+
+    [Header("Settings")]
+    public float WaitingTimeInSeconds = 4f;
+    public float RotationAmountInDegrees = 45f;
+    public float SpinDurationInSeconds = 4f;
+    public AnimationCurve RotationCurve;
+
+    void Start() {
+        StartCoroutine("RotateQuadPlatform");
+    }
+
+    IEnumerator RotateQuadPlatform() {
+        yield return new WaitForSeconds(WaitingTimeInSeconds);
+
+        Quaternion startRotation = RotatingQuadPlatform.transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0f, RotationAmountInDegrees, 0f);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < SpinDurationInSeconds) {
+            float fraction = RotationCurve.Evaluate((elapsedTime / SpinDurationInSeconds));
+            Quaternion newRotation = Quaternion.Lerp(startRotation, endRotation, fraction);
+            RotatingQuadPlatform.rotation = newRotation;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        RotatingQuadPlatform.rotation = endRotation;
+
+        yield return StartCoroutine("RotateQuadPlatform");
+    }
+}
---
+++ CaptainToadPrototype/Assets/Scripts/Mechanics/PutMainCharacterOnPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PutMainCharacterOnPlatform : MonoBehaviour {
+    public Transform Platform;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "MainCharacter") {
+            other.gameObject.transform.parent = Platform;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "MainCharacter") {
+            other.gameObject.transform.parent = null;
+        }
+    }
+}
