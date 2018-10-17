7715b9d18c83ff8e27f5880c00f89cdc67410d92
Sander <Sanderdrupal@gmail.com>
2018-10-17 21:26:27 +0200

    - Added pipe mechanic to level 3 (finished level now, V1)

--- CaptainToadPrototype/Assets/Scripts/Mechanics/TubePiece.cs
+++ CaptainToadPrototype/Assets/Scripts/Mechanics/TubePiece.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class TubePiece : MonoBehaviour {
     private MeshRenderer _tubeMeshRenderer;
 
     void Awake() {
-        _tubeMeshRenderer = this.transform.Find("Tube").GetComponent<MeshRenderer>();
+        GameObject Tube = this.transform.Find("Tube").gameObject;
+
+        Assert.IsNotNull(Tube, "No TUBE found, make sure there is a object named Tube in each of your pipes");
+
+        _tubeMeshRenderer = Tube.GetComponent<MeshRenderer>();
     }
 
     public void SwitchMaterial(Material newMaterial) {
