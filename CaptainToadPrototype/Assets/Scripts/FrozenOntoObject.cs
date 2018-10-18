88d08d71df07827f59dd786de4d4be530b7f07d1
Sander <Sanderdrupal@gmail.com>
2018-10-18 23:09:26 +0200

    - Added postprocessing - Added always visible shader - Made some changes to level 3

---
+++ CaptainToadPrototype/Assets/Scripts/FrozenOntoObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrozenOntoObject : MonoBehaviour {
+    public BoxCollider FreezedObjectCollider;
+    private Rigidbody _rb;
+
+    void Start() {
+        _rb = this.GetComponent<Rigidbody>();    
+    }
+
+    private void Update() {
+        if (!FreezedObjectCollider.enabled) {
+            _rb.isKinematic = false;
+            _rb.useGravity = true;
+        }
+    }
+
+}
